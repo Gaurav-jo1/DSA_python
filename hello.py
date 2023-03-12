c84feee1b143f51e7e701e70b498fe12910559b9
Gaurav Joshi <gj451026@gmail.com>
2023-03-12 19:19:02 +0530

    search in rotated array

--- hello.py
+++ hello.py
@@ -143,3 +143,50 @@
 
 # print(main())
 
+# _____________________________________________________________
+
+# Search in Rotated sorted array
+
+
+
+def main():
+    arr = [7, 8, 9, 1, 2, 3, 4, 5, 6]
+    # arr = [3,4,5,6,7,0,1,2]
+    target = 6
+
+    high = highN(arr)
+
+    firstHalf = findT(arr, target, 0, high)
+
+    if firstHalf != -1:
+        return firstHalf
+    else:
+        return findT(arr, target, high + 1, len(arr) - 1)
+
+def highN(arr):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = start + (end - start) // 2
+        if arr[start] > arr[mid]:
+            end = mid
+        elif arr[start] < arr[mid]:
+            start = mid
+        else:
+            return mid
+
+def findT(arr, target, start, end):
+    starting = start
+    ending = end
+
+    while starting <= ending:
+        mid = (starting + ending) // 2
+        if arr[mid] == target:
+            return mid
+        elif target > arr[mid]  :
+            starting = mid + 1
+        elif target < arr[mid]:
+            ending = mid - 1 
+    return - 1
+
+print(main())
