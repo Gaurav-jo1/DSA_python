4a3e70766d90360921251f7a907f446593cc0d90
Gaurav Joshi <gj451026@gmail.com>
2023-03-18 16:07:59 +0530

    selection sort simplier way added

---
+++ selectio0n_sort/sort.py
@@ -0,0 +1,8 @@
+def insertion_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+print(insertion_sort([5, 4, 2, 1]))
