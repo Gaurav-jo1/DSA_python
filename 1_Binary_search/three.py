a3d958bd04202fde0644ece1e7dd50a6c2868152
Gaurav Joshi <gj451026@gmail.com>
2023-04-08 20:27:26 +0530

    Peak Index in a Mountain Array

--- 1_Binary_search/three.py
+++ 1_Binary_search/three.py
@@ -1,2 +1,19 @@
 # Q - Peak Index in a Mountain Array
 
+def binary_search(arr):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid - 1] < arr[mid] > arr[mid + 1]:
+            return arr[mid]
+        elif arr[mid] > arr[mid - 1]:
+            start = mid 
+        else:
+            end = mid
+     
+
+# print(binary_search([2, 4, 5, 6, 7, 9, 11, 13, 6, 3, 2, 1]))
+print(binary_search([0,10,5,2]))
+
+# LeetCode - https://leetcode.com/problems/peak-index-in-a-mountain-array/
