80b28a4b317db74a7ad1f8ac23a3773696d781ec
Gaurav-jo1 <gj451026@gmail.com>
2023-11-23 16:23:16 +0530

    graph directory added

--- Python101/List_Comprehensions.py
+++ Python101/List_Comprehensions.py
@@ -1,5 +1,7 @@
-x = [i for i in range(10)]
-y = [[j for j in range(5)] for i in range(10)]
+x = [i for i in range(1, 11)]
+y = [[j for j in range(5)] for _ in range(10)]
 z = [i for i in range(10) if i % 2 == 0]
 
 print(z)
+# print(z)
+# print(z)
--- Python101/fun_argument.py
+++ Python101/fun_argument.py
@@ -2,8 +2,11 @@
 
 
 def complicated_function1(x, y, z=None):
-    print(x, y)
+    print(x, y, z)
     pass
+
+
+# complicated_function1(1, 4)
 
 
 # What it does is accept any number of positional argumnets.
@@ -11,6 +14,9 @@ def complicated_function1(x, y, z=None):
 def complicated_function2(x, y, *args):
     print(x, y, args)
     pass
+
+
+# complicated_function2(1, 2, 5, 6, 2, 5, 1)
 
 
 # **kwargs means we accept any key word arguments.
@@ -21,9 +27,15 @@ def complicated_function3(*args, **kwargs):
     pass
 
 
+# complicated_function3(x=1, z="Hello", b=True)
+
+
 def complicated_function4(*args, **kwargs):
     print(args, kwargs)
     pass
+
+
+# complicated_function4(1, 2, 4, 5, x=1, z="Hello", b=True)
 
 
 def complicated_function5(a, b, c=True, d=False):
@@ -31,8 +43,4 @@ def complicated_function5(a, b, c=True, d=False):
     pass
 
 
-complicated_function1(1, 4)
-complicated_function2(1, 2, 5, 6, 2, 5, 1)
-complicated_function3(x=1, z="Hello", b=True)
-complicated_function4(1, 2, 4, 5, x=1, z="Hello", b=True)
-complicated_function5(*[1,2 ], **{"c": "hello", "d": "cool"}) 
+complicated_function5(*[1, 2], **{"c": "hello", "d": "cool"})
