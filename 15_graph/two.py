9595ed744325b7a5ea73cf3952bee25e4fb2fd2e
Gaurav-jo1 <gj451026@gmail.com>
2023-11-24 14:40:06 +0530

    Breath First Search

--- 15_graph/two.py
+++ 15_graph/two.py
@@ -1,1 +1,32 @@
-# breadth First: Queue
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, vertex, edges):
+        self.graph[vertex] = edges
+
+    def bfs(self, start):
+        visited = set()
+        queue = [start]
+        visited.add(start)
+
+        while queue:
+            current_vertex = queue.pop(0)
+            print(current_vertex, end=' ')
+
+            for neighbor in self.graph[current_vertex]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+
+# Example usage:
+graph = Graph()
+graph.add_edge(1, [2, 3])
+graph.add_edge(2, [4, 5])
+graph.add_edge(3, [])
+graph.add_edge(4, [])
+graph.add_edge(5, [])
+
+print("BFS starting from vertex 1:")
+graph.bfs(1)
+print("")
