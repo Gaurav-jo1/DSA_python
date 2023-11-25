10b2109ef7ecc581aed261e691d931b70bc1995d
Gaurav-jo1 <gj451026@gmail.com>
2023-11-25 20:08:14 +0530

    Undirect Path

--- 15_graph/README.md
+++ 15_graph/README.md
@@ -1,3 +1,9 @@
+## one.py - Depth First
+## two.py - Breath First
+## three.py - Has Path
+## four.py - Undirected Path
+
+
 ![Screenshot from 2023-11-23 09-32-23](https://github.com/DevGauravJoshi/DSA_Python/assets/93304640/9a51718a-eab2-4f3c-b23a-d047d3ba7dec)
 
 
---
+++ 15_graph/four.py
@@ -0,0 +1,54 @@
+# Undirect Path
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+        self.visited = set()
+
+    def edges_to_graph(self, edges):
+        for edge in edges:
+            if len(edge) < 2:
+                self.graph[edge[0]] = []
+                continue
+
+            vertex1, vertex2 = edge
+
+            # Adding vertex1 to vertex2's adjacency list
+            if vertex1 in self.graph:
+                self.graph[vertex1].append(vertex2)
+            else:
+                self.graph[vertex1] = [vertex2]
+
+            # Adding vertex2 to vertex1's adjacency list
+            if vertex2 in self.graph:
+                self.graph[vertex2].append(vertex1)
+            else:
+                self.graph[vertex2] = [vertex1]
+
+    def dfs(self, src, dst):
+        self.visited.add(src)
+
+        if src == dst:
+            return True
+
+        for neighbor in self.graph[src]:
+            if neighbor not in self.visited:
+                if self.dfs(neighbor, dst):
+                    return True
+        return False
+
+
+# edges = [["i", "j"], ["k", "i"], ["m", "k"], ["k", "l"], ["o", "n"]]
+edges = [
+    ["b", "a"],
+    ["c", "a"],
+    ["b", "c"],
+    ["q", "r"],
+    ["q", "s"],
+    ["q", "u"],
+    ["q", "t"],
+]
+graph = Graph()
+graph.edges_to_graph(edges)
+
+print(graph.dfs("a", "b"))
