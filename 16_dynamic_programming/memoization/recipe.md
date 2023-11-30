4be98efd6406b0ca3462f1ed62fa3bf65ebd1c91
Gaurav-jo1 <gj451026@gmail.com>
2023-11-30 11:58:38 +0530

    canSum(targetSum, numbers)

---
+++ 16_dynamic_programming/memoization/recipe.md
@@ -0,0 +1,15 @@
+# 1. Make it work
+
+visualise the problem as tree.
+
+Implement the tree using recursion.
+
+Test it (There is difference between wrong code & slow code)
+
+# 2. Make it effiecient
+
+Add a memo object/ dict
+
+Add a base case to return memo values
+
+Store return value into the memo
--- 16_dynamic_programming/memoization/three.py
+++ 16_dynamic_programming/memoization/three.py
@@ -1,1 +1,42 @@
-# 
+# Can Sum
+
+# Write a function `canSum(targetSum, numbers)` that takes in a targetSum and an array of numbers as arguments.
+
+# The function should return a boolean indicating whether or not it is possible to generate the targetSum using numbers from the array.
+
+# You may use an element of the array as many times as needed.
+
+# You may assume that all input numbers are nonnegative.
+
+
+class CanSum:
+    def __init__(self) -> None:
+        self.memo = {}
+
+    def start(self, targetSum: int, numbers: list[int]) -> None:
+        if targetSum == 0:
+            return True
+        
+        if targetSum < 0:
+            return False
+
+        if targetSum in self.memo:
+            return self.memo[targetSum]
+
+        for number in numbers:
+            if number <= targetSum:
+                child = self.start(targetSum - number, numbers)
+                self.memo[targetSum - number] = child
+
+                if child == True:
+                    return True
+
+        return False
+
+
+can_sum = CanSum()
+# print(can_sum.start(7, numbers=[2, 3]))
+# print(can_sum.start(7, numbers=[5, 3, 4, 7]))
+# print(can_sum.start(7, numbers=[2, 4]))
+# print(can_sum.start(8, numbers=[2, 3, 5]))
+print(can_sum.start(300, numbers=[7, 14]))
